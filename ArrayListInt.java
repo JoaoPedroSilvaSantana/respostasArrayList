b78c268317379d56dc622810b7498dd8af7ba292
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-02 16:27:50 -0300

    Create ArrayListInt.java

---
+++ ArrayListInt.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class ArrayListInt {
+	public static void main(String [] args) {
+		ArrayList<Integer> idades = new ArrayList<>();
+		
+		idades.add(17);
+		idades.add(17);
+		idades.add(18);
+		idades.add(17);
+		
+		System.out.println(idades);
+	}
+}
