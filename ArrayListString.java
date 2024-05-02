17093ba703e80598dbb9ec3c76800a1f1916d26b
JoaoPedroSilvaSantana <166645046+JoaoPedroSilvaSantana@users.noreply.github.com>
2024-05-02 16:18:56 -0300

    Create ArrayListString.java

---
+++ ArrayListString.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+	public static void main(String [] args) {
+		ArrayList<String> nomes = new ArrayList<>();
+		
+		nomes.add("Jp");
+		nomes.add("Leticia");
+		nomes.add("Danillo");
+		nomes.add("Lopes");
+		
+		System.out.println(nomes);
+	}
+}
